4bdfb4d582bc1a7a99c025286cafa8116f7bf650
プライベート <roiti46@gmail.com>
2015-05-30 22:56:58 +0900

    add template source code

---
+++ gcj/template.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define REP(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define rep(i, a) REP(i, 0, a)
+
+
+int main(void) {
+    int T;
+    cin >> T;
+    while (T--) {
+
+        printf("Case %d: %d", loop, ans);
+    }
+
+    return 0;
+}
---
+++ gcj/template.py
@@ -0,0 +1,7 @@
+import sys, math, re, itertools as it, bisect, string
+from collections import (defaultdict, deque)
+
+T = int(raw_input())
+for case in xrange(1, T + 1):
+
+    print "Case #%d: %d" % (case, ans)
